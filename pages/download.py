67e9ac8c8b4b501f40b3f97d49d58543b44d830a
CKHope <chungkien20211011@gmail.com>
2023-08-30 20:34:46 +0700

    ud

--- pages/download.py
+++ pages/download.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 if len(st.session_state.df)>0:
     st.success('Got data')
+    st.dataframe(st.session_state.df)
     df=st.session_state.df
     csv=df.to_csv(index=False).encode('utf-8')
     st.download_button(
