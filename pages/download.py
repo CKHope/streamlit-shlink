b65ae21a459ba5a4b4eaec520d02745c415e71cd
CKHope <chungkien20211011@gmail.com>
2023-08-30 16:51:16 +0700

    ud

--- pages/download.py
+++ pages/download.py
@@ -4,7 +4,7 @@ st.title(
     "Download the result here"
 )
 
-with open(f"resutl.csv", "rb") as file:
+with open(f"result.csv", "rb") as file:
     btn = st.download_button(
         label=f"Download result",
         data=file,
