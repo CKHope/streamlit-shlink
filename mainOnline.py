9cf178532974761fd1fa148f23e1b853757294c7
CKHope <chungkien20211011@gmail.com>
2023-08-30 17:42:46 +0700

    ud

--- mainOnline.py
+++ mainOnline.py
@@ -69,7 +69,7 @@ def process_urls_in_batches(api_key, urls, tags_list, crawlable, forward_query, 
 def main():
     st.title("URL Shortener")
 
-    api_key = '341126f1-0838-4ff7-a52b-df728d95c463'
+    api_key = '4f53fddb-eb10-4a27-9808-5a8f72ac06ed'
     crawlable = False
     forward_query = False
     short_code_length = 9
