33ef85ae14a96480e4bf54ae16961cc98a2a5bb9
CKHope <chungkien20211011@gmail.com>
2023-12-27 00:55:32 +0700

    ud

--- mainOnline.py
+++ mainOnline.py
@@ -208,8 +208,8 @@ def main():
         except Exception as e:
             st.error(f"Error: {e}")
         
-        # st.write(f"Total runtime: {total_time:.2f} seconds")
-        # st.write(f"Average time per link: {total_time / len(urls):.4f} seconds")
+        st.write(f"Total runtime: {total_time:.2f} seconds")
+        st.write(f"Average time per link: {total_time / len(urls):.4f} seconds")
     # uploaded_file=st.empty()
         
 if __name__ == "__main__":
